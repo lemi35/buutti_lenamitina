c3dd93a056ac82e4a7253cd43ed1d3be7ddcaac7
Lena <83163194+lemi35@users.noreply.github.com>
2023-12-18 11:29:37 +0200

    Add files via upload

---
+++ as1,2,3,4.js
@@ -0,0 +1,60 @@
+// Assignment 1
+
+const userName="Juliet"
+
+// this is a comment
+// we greet the user
+
+console.log("Hello," + " " + userName);
+
+// Assignment 2
+
+// First print 5 and then print 7
+const number1 = 5
+console.log(number1);
+const number2 = 7;
+console.log(number2)
+
+// Assignment 3
+
+let value = 2;
+// console.log(value);
+
+const increase = 3;
+// console.log(increase);
+
+value = value + increase;
+// console.log(value);
+// console.log(increase);
+
+// const newValue = value + increase;
+// console.log(newValue);
+// console.log(increase);
+value = value + increase;
+
+const increaseMinusOne = increase - 1;
+value = value + increaseMinusOne;
+console.log("result: " + value);
+// console.log(increaseMinusOne);
+
+// Assignment 4
+
+let number = 5;
+const increase2 = 4;
+var limit = 11
+
+number = number + increase2
+
+// Print number if it is bigger than limit (11)
+
+if (number > limit) {
+     console.log(number);
+}
+
+
+number = number + increase
+
+// Check if it is bigger after it has been increased again
+if (number > limit)  { 
+    console.log(number);
+};
